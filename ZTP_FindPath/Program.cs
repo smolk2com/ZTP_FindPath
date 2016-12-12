5ed5ea6608d160ac40d9b6a5520c2df4115c6736
Grzegorz Smolarski <smolk2com@gmail.com>
2016-12-12 10:18:09 +0100

    read xml

--- ZTP_FindPath/Program.cs
+++ ZTP_FindPath/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace ZTP_FindPath
 {
@@ -10,6 +11,18 @@ namespace ZTP_FindPath
     {
         static void Main(string[] args)
         {
+            ReadConfigXml();
+        }
+        static void ReadConfigXml()
+        {
+            XDocument xdoc;
+            xdoc = XDocument.Load(Environment.CurrentDirectory + @"\path.xml");
+
+            List<double> IDsInDistantXML = xdoc.Root.Element("trkseg")
+                                     .Elements("trkpt")
+                                     .Select(pr => (double)pr.Attribute("lon"))
+                                     .ToList();
+
         }
     }
 }
