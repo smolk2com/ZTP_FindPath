9b2701e6b86261c9b590d62f4ed354a3067be745
Grzegorz Smolarski <smolk2com@gmail.com>
2017-01-27 12:48:06 +0100

    Cd. poprawek

--- ZTP_PathLibrary/Paramethers.cs
+++ ZTP_PathLibrary/Paramethers.cs
@@ -9,14 +9,15 @@ namespace ZTP_PathLibrary
 {
     public class Points
     {
-        //private double _lon;
-        //private double _lat;
-        //private double _ele;
-        //private DateTime _time;
-        public double lon { get; set; }
-        public double lat { get; set; }
-        public double ele { get; set; }
-        public DateTime time { get; set; }     
+        private double _lon;
+        private double _lat;
+        private double _ele;
+        private DateTime _time;
+
+        public double lon { get { return _lon; } set { _lon = value; } }
+        public double lat { get { return _lat; } set { _lat = value; } }
+        public double ele { get { return _ele; } set { _ele = value; } }
+        public DateTime time { get { return _time; } set { _time = value; } }
     }
     public class Paramethers
     {
@@ -46,23 +47,5 @@ namespace ZTP_PathLibrary
             }
             return _allPoints;
         }
-
-        //public Paramethers()
-        //{
-        //    XDocument xdoc;
-        //    xdoc = XDocument.Load(Environment.CurrentDirectory + @"\" + @"../../../path.gpx");
-        //    XNamespace gpx = XNamespace.Get("http://www.topografix.com/GPX/1/1");
-        //    this._Longitude = xdoc.Descendants(gpx + "trkpt").Select(lon => (double)lon.Attribute("lon")).ToList();
-        //    this._Latitude = xdoc.Descendants(gpx + "trkpt").Select(lat => (double)lat.Attribute("lat")).ToList();
-        //    this._Height = xdoc.Descendants(gpx + "trkpt").Select(ele => (double)ele.Element(gpx + "ele")).ToList();
-        //    this._AllTimes = xdoc.Descendants(gpx + "trkpt").Select(t => (DateTime)t.Element(gpx + "time")).ToList();
-        //}
-       
-
-
-        //public List<double> Longitude { get { return _Longitude; } }
-        //public List<double> Latitude { get { return _Latitude; } }
-        //public List<DateTime> AllTimes { get { return _AllTimes; } }
-        //public List<double> Height { get { return _Height; } }
     }
 }
